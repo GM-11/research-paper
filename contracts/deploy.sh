e9d566d989d14bd087d5b4f4eae88e912b254719
GM-11 <mathuyrgopal@gmail.com>
2024-08-25 20:45:48 +0000

    added users contract

--- contracts/deploy.sh
+++ contracts/deploy.sh
@@ -7,15 +7,17 @@ read -p "Enter network (local / prod): " network
 if [ "$network" == "local" ]; then
 echo "Deploying Project Factory Contract"
 forge create ./src/ProjectFactory.sol:ProjectFactory --rpc-url http://127.0.0.1:8545 --private-key 0x2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6
+echo "Deploying Users Contract"
+forge create ./src/Users.sol:Users --rpc-url http://127.0.0.1:8545 --private-key 0x2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6
 echo ""
 else
     echo "Invalid network type. Please enter 'local' or 'prod'."
 fi
 
 
-cast send 0x8ce361602B935680E8DeC218b820ff5056BeB7af "createProject(string name, string paperUri, string topic)" "Bio" "URI" "sometopic" --rpc-url http://127.0.0.1:8545 --private-key 0x4bbbf85ce3377467afe5d46f804f221813b2bb87f24d81f60f1fcdbf7cbf4356
-cast send 0x8ce361602B935680E8DeC218b820ff5056BeB7af "createProject(string name, string paperUri, string topic)" "THermo" "URI" "sometopic" --rpc-url http://127.0.0.1:8545 --private-key 0x4bbbf85ce3377467afe5d46f804f221813b2bb87f24d81f60f1fcdbf7cbf4356
-cast send 0x8ce361602B935680E8DeC218b820ff5056BeB7af "createProject(string name, string paperUri, string topic)" "Eng" "URI" "sometopic" --rpc-url http://127.0.0.1:8545 --private-key 0x4bbbf85ce3377467afe5d46f804f221813b2bb87f24d81f60f1fcdbf7cbf4356
+# cast send 0x8ce361602B935680E8DeC218b820ff5056BeB7af "createProject(string name, string paperUri, string topic)" "Bio" "URI" "sometopic" --rpc-url http://127.0.0.1:8545 --private-key 0x4bbbf85ce3377467afe5d46f804f221813b2bb87f24d81f60f1fcdbf7cbf4356
+# cast send 0x8ce361602B935680E8DeC218b820ff5056BeB7af "createProject(string name, string paperUri, string topic)" "THermo" "URI" "sometopic" --rpc-url http://127.0.0.1:8545 --private-key 0x4bbbf85ce3377467afe5d46f804f221813b2bb87f24d81f60f1fcdbf7cbf4356
+# cast send 0x8ce361602B935680E8DeC218b820ff5056BeB7af "createProject(string name, string paperUri, string topic)" "Eng" "URI" "sometopic" --rpc-url http://127.0.0.1:8545 --private-key 0x4bbbf85ce3377467afe5d46f804f221813b2bb87f24d81f60f1fcdbf7cbf4356
 
 
 
